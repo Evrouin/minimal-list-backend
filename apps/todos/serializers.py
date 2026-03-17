ba8a3fdb8bda61162ca2812062943c5898da23b3
Evrouin <elwaycortez@gmail.com>
2026-03-18 01:39:10 +0800

    feat: add image validator

--- apps/todos/serializers.py
+++ apps/todos/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
 from .models import Todo
+from .utils import process_image
+
+MAX_UPLOAD_SIZE = 5 * 1024 * 1024  # 5MB
 
 
 class TodoSerializer(serializers.ModelSerializer):
@@ -8,5 +11,22 @@ class TodoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Todo
-        fields = ["id", "title", "body", "image", "completed", "deleted", "pinned", "created_at", "updated_at"]
-        read_only_fields = ["id", "created_at", "updated_at"]
+        fields = ["id", "title", "body", "image", "thumbnail", "completed", "deleted", "pinned", "created_at", "updated_at"]
+        read_only_fields = ["id", "thumbnail", "created_at", "updated_at"]
+
+    def validate_image(self, value):
+        if value and value.size > MAX_UPLOAD_SIZE:
+            raise serializers.ValidationError("Image must be under 5MB.")
+        return value
+
+    def create(self, validated_data):
+        image = validated_data.get("image")
+        if image:
+            validated_data["image"], validated_data["thumbnail"] = process_image(image)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        image = validated_data.get("image")
+        if image:
+            validated_data["image"], validated_data["thumbnail"] = process_image(image)
+        return super().update(instance, validated_data)
