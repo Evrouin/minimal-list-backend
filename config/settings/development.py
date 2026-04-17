0a3ea041c0c0704a51f29a56831c8bf2f60396b4
Evrouin <elwaycortez@gmail.com>
2026-04-18 03:04:18 +0800

    feat: add encryption key and hmac signing as optional for debugging

--- config/settings/development.py
+++ config/settings/development.py
@@ -26,3 +26,6 @@ EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+
+ENCRYPTION_KEY = config("ENCRYPTION_KEY", default="")
+HMAC_SIGNING_KEY = config("HMAC_SIGNING_KEY", default="")
